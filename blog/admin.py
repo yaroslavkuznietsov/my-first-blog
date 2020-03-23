42f94a21d93c4aa429af7cdd1d36452e84cece96
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2020-03-23 10:51:22 +0200

    Changed the HTML for the site

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+
 admin.site.register(Post)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+#from django.http import Response
+
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
